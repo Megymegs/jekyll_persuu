f6113b5b321a6a62879956f866f382af521ef3d8
Megan Meyer <33827558+Megymegs@users.noreply.github.com>
2022-11-06 22:51:47 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$("#palette_1").on('click', function(){
+    $('#homepage-main').removeClass().addClass('palette_1');
+    $('#pattern').parent('.col-3').removeClass('d-none');
+    $("#linear").on('click', function(){
+        $('#homepage-main').removeClass('conic').addClass('linear');
+    });
+    $("#conic").on('click', function(){
+        $('#homepage-main').removeClass('linear').addClass('conic');
+    });
+});
+$("#palette_2").on('click', function(){
+    $('#homepage-main').removeClass().addClass('palette_2');
+    $('#pattern').parent('.col-3').removeClass('d-none');
+    $("#linear").on('click', function(){
+        $('#homepage-main').removeClass('conic').addClass('linear');
+    });
+    $("#conic").on('click', function(){
+        $('#homepage-main').removeClass('linear').addClass('conic');
+    });
+});
+$("#palette_3").on('click', function(){
+    $('#homepage-main').removeClass().addClass('palette_3');
+    $('#pattern').parent('.col-3').removeClass('d-none');
+    $("#linear").on('click', function(){
+        $('#homepage-main').removeClass('conic').addClass('linear');
+    });
+    $("#conic").on('click', function(){
+        $('#homepage-main').removeClass('linear').addClass('conic');
+    });
+});
+$("#palette_4").on('click', function(){
+    $('#homepage-main').removeClass().addClass('palette_4');
+    $('#pattern').parent('.col-3').removeClass('d-none');
+    $("#linear").on('click', function(){
+        $('#homepage-main').removeClass('conic').addClass('linear');
+    });
+    $("#conic").on('click', function(){
+        $('#homepage-main').removeClass('linear').addClass('conic');
+    });
+});
+$("#palette_5").on('click', function(){
+    $('#homepage-main').removeClass().addClass('palette_5');
+    $('#pattern').parent('.col-3').removeClass('d-none');
+    $("#linear").on('click', function(){
+        $('#homepage-main').removeClass('conic').addClass('linear');
+    });
+    $("#conic").on('click', function(){
+        $('#homepage-main').removeClass('linear').addClass('conic');
+    });
+});
+$("#palette_6").on('click', function(){
+    $('#homepage-main').removeClass().addClass('palette_6');
+    $('#pattern').parent('.col-3').removeClass('d-none');
+    $("#linear").on('click', function(){
+        $('#homepage-main').removeClass('conic').addClass('linear');
+    });
+    $("#conic").on('click', function(){
+        $('#homepage-main').removeClass('linear').addClass('conic');
+    });
+});
