5762cd521af4b43fb53953f3abc4abfeabd9306a
megymegs <33827558+Megymegs@users.noreply.github.com>
2022-11-13 23:59:30 +0200

    Updating landing, through, sign up and login pages

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,34 +1,33 @@
 // call updateQuote once when page loads
 var quote = $(".quote-content span");
 var cite = $(".quote-source em");
-async function updateLifeQuote() {
+async function updateInspirationalQuote() {
     // Fetch a random quote from the Quotable API
-    var responseFriendship = await fetch("https://api.quotable.io/random?tags=life");
+    var responseFriendship = await fetch("https://api.quotable.io/random?tags=inspirational");
     var data = await responseFriendship.json();
     if (responseFriendship.ok) {
         // Update DOM elements
         quote.text(data.content);
         cite.text("- " + data.author);
-        $('#quote').removeClass('love-quoted friendship-quoted').addClass('life-quoted');
+        $('#quote').removeClass('love-quoted friendship-quoted').addClass('inspiration-quoted');
     } else {
         location.reload();
-        $('#quote').removeClass('life-quoted love-quoted friendship-quoted');
+        $('#quote').removeClass('inspiration-quoted love-quoted friendship-quoted');
     }
 }
-updateLifeQuote();
+updateInspirationalQuote();
 setTimeout(() => {
-    if ($('#quote').hasClass('life-quoted')) {
+    if ($('#quote').hasClass('inspiration-quoted')) {
         setInterval(() => {
-            updateLifeQuote();
-        }, 10000);
+            updateInspirationalQuote();
+        }, 13000);
     }
-}, 10000);
+}, 13000);
 $("#romance").on('click', function(){
     $('body').removeClass().addClass('default');
-    $('#landing').css('transform', "none");
-    $('#favicon_ico').attr('href','https://megymegs.github.io/jekyll_persuu/assets/favicon/favicon_1.ico');
+    $('#favicon_ico').attr('href','https://megymegs.github.io/jekyll_persuu/assets/favicon/persuu_logo.ico');
     $('#favicon_png').attr('href','https://megymegs.github.io/jekyll_persuu/assets/favicon/favicon_1.png');
-    $('.member_type').removeClass('d-none').siblings('.acc_type').addClass('d-none');
+    $('body main .brand_ID a .persuu img').attr('src','https://megymegs.github.io/jekyll_persuu/assets/logo/persuu_logo.png');
     async function updateLoveQuote() {
         // Fetch a random quote from the Quotable API
         var responseLove = await fetch("https://api.quotable.io/random?tags=love");
@@ -37,25 +36,24 @@ $("#romance").on('click', function(){
             // Update DOM elements
             quote.text(data.content);
             cite.text("- " + data.author);
-            $('#quote').removeClass('life-quoted friendship-quoted').addClass('love-quoted');
+            $('#quote').removeClass('inspiration-quoted friendship-quoted').addClass('love-quoted');
         } else {
             location.reload();
-            $('#quote').removeClass('life-quoted love-quoted friendship-quoted');
+            $('#quote').removeClass('inspiration-quoted love-quoted friendship-quoted');
         }
     }
     updateLoveQuote();
     if ($('#quote').hasClass('love-quoted')) {
         setInterval(() => {
             updateLoveQuote();
-        }, 10000);
+        }, 13000);
     }
 });
 $("#companion").on('click', function(){
     $('body').removeClass().addClass('recreational');
-    $('#landing').css('transform', "none");
-    $('#favicon_ico').attr('href','https://megymegs.github.io/jekyll_persuu/assets/favicon/favicon_2.ico');
+    $('#favicon_ico').attr('href','https://megymegs.github.io/jekyll_persuu/assets/favicon/persuu_logo_alt.ico');
     $('#favicon_png').attr('href','https://megymegs.github.io/jekyll_persuu/assets/favicon/favicon_2.png');
-    $('.member_type').removeClass('d-none').siblings('.acc_type').addClass('d-none');
+    $('body main .brand_ID a .persuu img').attr('src','https://megymegs.github.io/jekyll_persuu/assets/logo/persuu_logo_alt.png');
     async function updateFriendshipQuote() {
         // Fetch a random quote from the Quotable API
         var responseFriendship = await fetch("https://api.quotable.io/random?tags=friendship");
@@ -64,21 +62,20 @@ $("#companion").on('click', function(){
             // Update DOM elements
             quote.text(data.content);
             cite.text("- " + data.author);
-            $('#quote').removeClass('life-quoted love-quoted').addClass('friendship-quoted');
+            $('#quote').removeClass('inspiration-quoted love-quoted').addClass('friendship-quoted');
         } else {
             location.reload();
-            $('#quote').removeClass('life-quoted love-quoted friendship-quoted');
+            $('#quote').removeClass('inspiration-quoted love-quoted friendship-quoted');
         }
     }
     updateFriendshipQuote();
     if ($('#quote').hasClass('friendship-quoted')) {
         setInterval(() => {
             updateFriendshipQuote();
-        }, 10000);
+        }, 13000);
     }
 });
-$('#back').on('click', function(){
-    $('#landing').css('transform', "translateY(5vw)");
+$('#landing #back').on('click', function(){
     $('.acc_type').removeClass('d-none').siblings('.member_type').addClass('d-none');
-    updateLifeQuote();
+    updateInspirationalQuote();
 });
